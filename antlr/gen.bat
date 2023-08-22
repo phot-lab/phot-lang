f6d781be64577d752048a752820813a5b722f498
魏振杰 <Kogler_S@outlook.com>
2023-08-23 01:01:29 +0800

    feat: 进一步优化代码生成脚本

--- antlr/gen.bat
+++ antlr/gen.bat
@@ -1,7 +1,14 @@
 @echo off
-@echo generate antlr parser for python.
+
+set "info=[32m"
+set "end=[0m"
+
+@echo %info%Generating antlr parser for python...%end%
+@echo Output dir is "../src/front/antlr".
 java -jar antlr-4.13.0-complete.jar -Dlanguage=Python3 Psl.g4 -no-listener -visitor -o "../src/front/antlr"
-@echo delete PslVisitor.py as it's of little use.
+@echo %info%Deleting PslVisitor.py as it's of little use...%end%
 del ..\src\front\antlr\PslVisitor.py
-@echo format python codes using pycharm.bat.
-pycharm.bat format -m *.py "../src/front/antlr" -allowDefaults
+@echo Deleted PslVisitor.py successfully.
+@echo %info%Formatting python codes using pycharm.bat...%end%
+pycharm.bat format -m *.py "../src/front/antlr" -allowDefaults | findstr /b "Formatting"
+@echo %info%All tasks completed.%end%
