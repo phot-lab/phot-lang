ae4ea798eee19a7003c8db21c54bb2c1a463da7b
魏振杰 <Kogler_S@outlook.com>
2023-08-27 00:21:24 +0800

    feat: 部分细节调整

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,26 @@
 
 using namespace antlr4;
 
-int main(int argc, const char * argv[]) {
-  std::chrono::high_resolution_clock::time_point startTime, endTime;
-  auto src = std::ifstream();
-  src.open("D:/科研项目/PhotLab/phot-lang/demo/prjs/roft/roft.psl");
-  ANTLRInputStream input(src);
-  PslLexer lexer(&input);
-  CommonTokenStream tokens(&lexer);
+int main(int argc, const char *argv[])
+{
+    std::chrono::high_resolution_clock::time_point startTime, endTime;
+    for (int i = 0; i < 10; i++)
+    {
+        startTime = std::chrono::high_resolution_clock::now();
+        auto src = std::ifstream();
+        src.open("D:/科研项目/PhotLab/phot-lang/demo/prjs/roft/roft.psl");
+        ANTLRInputStream input(src);
+        PslLexer lexer(&input);
+        CommonTokenStream tokens(&lexer);
+        PslParser parser(&tokens);
+        tree::ParseTree *tree = parser.program();
+        endTime = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+        std::cout << "函数运行时间：" << duration << " 毫秒" << std::endl;
+    }
 
-  startTime = std::chrono::high_resolution_clock::now();
-  PslParser parser(&tokens);
-  tree::ParseTree *tree = parser.program();
+    // auto s = tree->toStringTree(&parser);
+    // std::cout << "Parse Tree: " << s << std::endl;
 
-  endTime = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
-
-  std::cout << "函数运行时间：" << duration << " 毫秒" << std::endl;
-
-  // auto s = tree->toStringTree(&parser);
-  // std::cout << "Parse Tree: " << s << std::endl;
-
-  return 0;
+    return 0;
 }
